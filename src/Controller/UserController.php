2cfea80963ace8bf73297fa778ec4cf6952ff16f
Shiramatsu-kitsune <ririchiyo.shirakiin77@gmail.com>
2025-01-30 09:03:58 -0300

    pour manel

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -25,24 +25,30 @@ class UserController extends AbstractController
 
      
     public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
-    {
-        if ($request->isMethod('POST')) {
-            $email = $request->request->get('email');
-            $password = $request->request->get('password');
-            $roles = (array) $request->request->get('roles', []); // Convertir en tableau
-
-            $user = new User();
-            $user->setEmail($email);
-            $user->setPassword($passwordHasher->hashPassword($user, $password));
-            $user->setRoles($roles); 
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('user_index');
-        }
-
-        return $this->render('user/new.html.twig');
+{
+    
+    if ($this->getUser()) {
+        return $this->redirectToRoute('user_index');
     }
+
+    if ($request->isMethod('POST')) {
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+        $roles = (array) $request->request->get('roles', []);
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setPassword($passwordHasher->hashPassword($user, $password));
+        $user->setRoles($roles);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_login'); 
+    }
+
+    return $this->render('user/new.html.twig');
+}
 
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
